099f94de5aadfa234de98566352781da9289ab3e
11 <alextatewalter@gmail.com>
2025-12-16 00:17:05 +0000

    Delete src/src/lib directory

--- src/src/lib/games.js
+++
@@ -1,9 +0,0 @@
-export const GAMES = [
-  { id: "speed_tap", name: "Speed Tap", description: "Click as many times as possible in 20 seconds." },
-  { id: "endurance", name: "Endurance", description: "Survive as many rounds as possible. Score = streak." },
-  { id: "word_lock", name: "Word Lock", description: "Write a caption under constraints. Score = rules passed + speed bonus." },
-];
-
-export function getGame(gameId) {
-  return GAMES.find((g) => g.id === gameId);
-}
